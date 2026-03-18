8a91ce37e3d4b30e4ee537fb53ab7d0a3877c7c2
김민성 <kms@ieetu.com>
2026-03-18 14:27:38 +0900

    커밋

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", async (e) => {
     });
 
     if (response.ok) {
-      message.textContent = "로그인 성공";
+      localStorage.setItem("isAdminLoggedIn", "true");
       window.location.href = "/github.io/admin/admin-dashboard.html";
     } else {
       message.textContent = "아이디 또는 비밀번호가 틀렸습니다.";
