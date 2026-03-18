eafff9c6f2df7b4e62f7358984a6418a8eed6f0d
김민성 <kms@ieetu.com>
2026-03-18 16:13:48 +0900

    커밋

--- assets/js/admin/board/board-insert.js
+++ assets/js/admin/board/board-insert.js
@@ -1,10 +1,12 @@
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
+const fileInput = document.getElementById("file");
 const saveBtn = document.getElementById("saveBtn");
 
 saveBtn.addEventListener("click", () => {
   const title = titleInput.value.trim();
   const content = contentInput.value.trim();
+  const file = fileInput.files[0];
 
   if (!title) {
     alert("제목을 입력하세요.");
@@ -16,29 +18,25 @@ saveBtn.addEventListener("click", () => {
     return;
   }
 
-  const posts = JSON.parse(localStorage.getItem("posts") || "[]");
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("content", content);
 
-  const newPost = {
-    title,
-    content,
-    date: new Date().toLocaleDateString(),
-  };
-
-  posts.unshift(newPost); // 최신글 위로
+  if (file) {
+    formData.append("file", file);
+  }
 
   fetch("http://localhost:8081/posts", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: title,
-      content: content,
-    }),
+    body: formData,
   })
     .then((res) => res.json())
     .then(() => {
       alert("저장되었습니다.");
       window.location.href = "/github.io/admin/board/list.html";
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("저장 실패");
     });
 });
