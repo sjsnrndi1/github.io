78593b6462ca6c462dbda49bcab2ebbfd5fa7de1
김민성 <kms@ieetu.com>
2026-05-07 13:27:13 +0900

    커밋

--- assets/js/admin/header.js
+++ assets/js/admin/header.js
@@ -10,21 +10,17 @@ fetch("/github.io/admin/header.html")
   .then((data) => {
     document.getElementById("header-container").innerHTML = data;
 
-    // 로그아웃 버튼 이벤트 다시 연결
     document.getElementById("logoutBtn").addEventListener("click", () => {
       localStorage.removeItem("isAdminLoggedIn");
       alert("로그아웃되었습니다.");
       location.href = "/github.io/admin/admin.html";
     });
 
-    // 현재 메뉴 활성화
     const currentPath = window.location.pathname;
-
     let currentPage = "";
 
     if (currentPath.includes("dashboard")) currentPage = "dashboard";
     else if (currentPath.includes("board")) currentPage = "board";
-    else if (currentPath.includes("users")) currentPage = "users";
 
     document.querySelectorAll(".admin-nav a").forEach((menu) => {
       if (menu.dataset.page === currentPage) {
