4d5077980d3cf29385c0be722d399bc4bffd838a
김민성 <kms@ieetu.com>
2026-05-08 15:07:01 +0900

    커밋

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,32 @@
 ﻿const menuToggle = document.getElementById("menu-toggle");
-      const navLinks = document.getElementById("nav-links");
+const navLinks = document.getElementById("nav-links");
 
-      menuToggle.addEventListener("click", () => {
-        const isActive = navLinks.classList.toggle("active");
-        menuToggle.setAttribute("aria-expanded", String(isActive));
-      });
+menuToggle.addEventListener("click", () => {
+  const isActive = navLinks.classList.toggle("active");
+  menuToggle.setAttribute("aria-expanded", String(isActive));
+});
 
-      navLinks.querySelectorAll("a").forEach((link) => {
-        link.addEventListener("click", () => {
-          navLinks.classList.remove("active");
-          menuToggle.setAttribute("aria-expanded", "false");
-        });
-      });
+navLinks.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menuToggle.setAttribute("aria-expanded", "false");
+  });
+});
+
+async function loadHeader() {
+  const response = await fetch("header.html");
+  const html = await response.text();
+
+  document.getElementById("header").innerHTML = html;
+}
+
+loadHeader();
+
+async function loadFooter() {
+  const response = await fetch("footer.html");
+  const html = await response.text();
+
+  document.getElementById("footer").innerHTML = html;
+}
+
+loadFooter();
